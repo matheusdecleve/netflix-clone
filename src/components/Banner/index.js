688982e1d84dade77afa81043022bf80152da038
Matheus Decleve <me@matheus.in>
2020-09-04 10:13:39 -0300

    Atualização do componente Banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "../../axios";
+import requests from "../../requests";
 
 function Banner() {
-  return <div></div>;
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+      }}
+    >
+      <div className="banner__content">
+        <h2>{movie?.title || movie?.name || movie?.original_name}</h2>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <p>{truncate(movie?.overview, 200)}</p>
+      </div>
+    </header>
+  );
 }
 
 export default Banner;
