53a5fe47ebf6ce61cba951069f818486a219a667
Matheus Decleve <me@matheus.in>
2020-09-09 00:04:56 -0300

    Atualização do componente Navbar

--- README.md
+++ README.md
@@ -2,15 +2,14 @@
 
 A clone of Netflix app with ReactJs.
 
-### Steps to
-
 Create an account in themoviedb.org and get the api key
 
-Create react app (npx create-react-app)
-
+### Install dependencies
 
 Install node-sass to preprocessor css (npm install node-sass)
 
 Install firebase-tools (npm install -g firebase-tools)
 
 Install axios (npm install axios)
+
+Install react-icons (npm install react-icons)
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,42 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import logo from '../../assets/netflix-logo.png';
+import React, { useEffect, useState } from "react";
+import { FaSearch, FaGift, FaBell } from "react-icons/fa";
 
-function Navbar(){
-	const [show, handleShow] = useState(false)
-	
-	useEffect(() => {
-		window.addEventListener("scroll", () => {
-			if(window.scrollY > 100){
-				handleShow(true)
-			} else {
-				handleShow(false)
-			}
-		});
-		return () => {
-			window.removeEventListener("scroll");
-		}
-	}, []);
+import avatar from "../../assets/avatar.png";
+import logo from "../../assets/netflix-logo.png";
 
-	return(
-		<div className={`navbar ${show && 'navbar__show'}`}>
-			<div className="navbar__left">
-				<img src={logo} alt="Logo Netflix Clone" title="Netflix Clone" />
-				<nav>
-					<a href="/">Início</a>
-					<a href="/">Séries</a>
-					<a href="/">Filmes</a>
-					<a href="/">Mais recentes</a>
-					<a href="/">Minha lista</a>
-				</nav>
-			</div>
-			<div className="navbar__right">
-				<a href="/">Lupa</a>
-				<a href="/">Present</a>
-				<a href="/">Not</a>
-				<a href="/">Avatar</a>
-			</div>
-		</div>
-	)
+function Navbar() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`navbar ${show && "navbar__show"}`}>
+      <div className="navbar__left">
+        <img src={logo} alt="Logo Netflix Clone" title="Netflix Clone" />
+        <nav>
+          <a href="/">Início</a>
+          <a href="/">Séries</a>
+          <a href="/">Filmes</a>
+          <a href="/">Mais recentes</a>
+          <a href="/">Minha lista</a>
+        </nav>
+      </div>
+      <div className="navbar__right">
+        <a href="/">
+          <FaSearch />
+        </a>
+        <a href="/">
+          <FaGift />
+        </a>
+        <a href="/">
+          <FaBell />
+        </a>
+        <a href="/">
+          <img src={avatar} alt="Foto do seu avatar" title="" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
