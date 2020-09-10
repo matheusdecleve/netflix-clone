f25e4a6af5ef0f33bc1e4d6fb515761901e04d9a
Matheus Decleve <me@matheus.in>
2020-09-09 23:14:52 -0300

    Adicionado nova request - fetchSearch

--- src/requests.js
+++ src/requests.js
@@ -9,6 +9,8 @@ const requests = {
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+
+  fetchSearch: `/search/movie?api_key=${API_KEY}&language=en-US&`,
 };
 
 export default requests;
